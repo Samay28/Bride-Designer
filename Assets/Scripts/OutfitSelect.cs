48fddce1ff7fc479beab5dc30da8cd6b71d05591
Samay28 <imsamaystar@gmail.com>
2023-06-03 11:32:18 +0530

    swipe

--- Assets/Scripts/OutfitSelect.cs
+++ Assets/Scripts/OutfitSelect.cs
@@ -5,19 +5,67 @@ using UnityEngine.UI;
 using TMPro;
 
 public class OutfitSelect : MonoBehaviour
-{   
+{
     public OutfitDatabase db;
     public SpriteRenderer SkinsUsed;
     private int selectedOption = 0;
     public Button[] select;
 
-    public void OnClickMe0()
-    {
-        updateOutfit(1);
-    }
+    //swipe
+    Vector2 startTouchPos;
+    Vector2 currentTouchPos;
+    Vector2 endTouchPos;
+    bool stopTouch = false;
+    public float swipeRange;
+    public float tapRange;
+
     private void updateOutfit(int selectedOption)
     {
         Outfit outfit = db.GetOutfit(selectedOption);
         SkinsUsed.sprite = outfit.OutfitLook;
     }
-}   
+    public void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPos = Input.GetTouch(0).position;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentTouchPos = Input.GetTouch(0).position;
+            Vector2 Distance = currentTouchPos - startTouchPos;
+
+            if (!stopTouch)
+            {
+                if (Distance.x < -swipeRange)
+                {
+                    if (selectedOption == 0)
+                        return;
+                    else
+                         selectedOption--;
+                        updateOutfit(selectedOption);
+                    stopTouch = true;
+                }
+                else if (Distance.x > swipeRange)
+                {
+                    if (selectedOption == 13)
+                        return;
+                    else
+                        selectedOption++;
+                    updateOutfit(selectedOption);
+                    stopTouch = true;
+                }
+            }
+        }
+        if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            stopTouch = false;
+            endTouchPos = Input.GetTouch(0).position;
+            Vector2 Distance = endTouchPos - startTouchPos;
+        }
+    }
+    private void Update()
+    {
+        Swipe();
+    }
+}
