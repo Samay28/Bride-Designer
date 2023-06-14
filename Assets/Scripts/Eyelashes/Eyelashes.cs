bf30c57f05b1beff04f15aaa20c8864ddfb9b6c0
Samay28 <imsamaystar@gmail.com>
2023-06-14 18:00:44 +0530

    makeup done

--- Assets/Scripts/Eyelashes/Eyelashes.cs
+++ Assets/Scripts/Eyelashes/Eyelashes.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Eyelashes : MonoBehaviour
+[System.Serializable]
+public class Eyelashes 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Sprite EyelashLook;
+    public int value;
 }
